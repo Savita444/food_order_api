f1103115c7ec23c35c251ee9a125ecea1f77e9c8
DhanuPatil0654 <dhananjaypatil572001@gmail.com>
2025-04-23 09:43:17 +0530

    changes are done

--- controllers/superadmin/TermConditionController.js
+++ controllers/superadmin/TermConditionController.js
@@ -1,7 +1,7 @@
 const termCondition = require("../../models/TermCondition");
 const logger = require("../../utils/logger");
 
-// Get all FAQs
+// Get all Term and Condition
 exports.getAllterm_Condition = async (req, res) => {
     try {
         const allFaq = await termCondition.find({ isDelete: 0 }).sort({ createdAt: -1 });
@@ -19,74 +19,8 @@ exports.getAllterm_Condition = async (req, res) => {
     }
 };
 
-// Create Term & Condition
-exports.createTermCondition = async (req, res) => {
-    try {
-        const { type, title,description } = req.body;
 
-        if (!type || !title || !description) {
-            return res.status(400).json({
-                success: false,
-                message: "Term and Condition are required",
-            });
-        }
-
-        const newtermCondtion = new termCondition({ type, title,description });
-        await newtermCondtion.save();
-
-        res.status(201).json({
-            success: true,
-            message: "Term And Condtions created successfully",
-            data: newtermCondtion,
-        });
-    } catch (error) {
-        logger.error(`Error creating Term Condition : ${error.message}`);
-        res.status(500).json({
-            success: false,
-            message: "Internal server error",
-        });
-    }
-};
-
-// Delete Terms and Condition
-exports.deleteTermCondition = async (req, res) => {
-    const { deleteTerm_id } = req.body;
-
-    if (!deleteTerm_id) {
-        return res.status(400).json({
-            success: false,
-            message: "Term and condition is required",
-        });
-    }
-
-    try {
-        const termconditionData = await termCondition.findById(deleteTerm_id);
-
-        if (!termconditionData) {
-            return res.status(404).json({
-                success: false,
-                message: "Term & Condition not found",
-            });
-        }
-
-        termconditionData.isDelete = 1;
-        await termconditionData.save();
-
-        res.status(200).json({
-            success: true,
-            message: "Term Condition deleted successfully",
-        });
-    } catch (err) {
-        logger.error(`Error deleting Term Condition: ${err.message}`);
-        res.status(500).json({
-            success: false,
-            message: "Internal server error",
-        });
-    }
-};
-
-
-// Update FAQ
+// Update Term and Condition
 exports.updateTermCondtion = async (req, res) => {
     const { _id } = req.params;
     const { type, title,description} = req.body;
@@ -135,55 +69,10 @@ exports.updateTermCondtion = async (req, res) => {
 };
 
 
-// Update FAQ Status
-exports.updateTermConditionStatus = async (req, res) => {
-    const { _id } = req.params;
-    const { isActive } = req.body;
-
-    if (!_id) {
-        return res.status(400).json({
-            success: false,
-            message: "Term & Condition is required",
-        });
-    }
-
-    const parsedStatus = parseInt(isActive);
-    if (![0, 1].includes(parsedStatus)) {
-        return res.status(400).json({
-            success: false,
-            message: "isActive must be 0 (inactive) or 1 (active)",
-        });
-    }
-
-    try {
-        const termConditionData = await termCondition.findById(_id);
-
-        if (!termConditionData || termConditionData.isDelete === 1) {
-            return res.status(404).json({
-                success: false,
-                message: "Term & Condition not found",
-            });
-        }
-
-        termConditionData.isActive = parsedStatus;
-        await termConditionData.save();
-
-        res.status(200).json({
-            success: true,
-            message: "Term & Condition status updated successfully",
-            data: termConditionData,
-        });
-    } catch (err) {
-        logger.error(`Error updating Term & Condition status: ${err.message}`);
-        res.status(500).json({
-            success: false,
-            message: "Internal server error",
-        });
-    }
-};
 
 
-// Get FAQ by ID
+
+// Get term and condition by ID
 exports.getTermConditionById = async (req, res) => {
     try {
         const { _id } = req.params;
--- routes/superadmin/TermconditionRoute.js
+++ routes/superadmin/TermconditionRoute.js
@@ -3,14 +3,11 @@ const router = express.Router();
 const termconditionController = require("../../controllers/superadmin/TermConditionController");
 const termCondtionValidator = require("../../validations/TermConditionValidator");
 
-// Static routes
-router.post("/add", termCondtionValidator.createtermConditionValidation , termconditionController.createTermCondition);
+//Get all data
 router.post("/list", termconditionController.getAllterm_Condition);
-router.post("/delete", termconditionController.deleteTermCondition);
+//Update Data 
 router.post("/update/:_id", termconditionController.updateTermCondtion);
-router.post("/status/:_id", termconditionController.updateTermConditionStatus);
-
-// Get by ID (Keep last)
+//Get by ID data
 router.post("/:_id", termconditionController.getTermConditionById);
 
 module.exports = router;
